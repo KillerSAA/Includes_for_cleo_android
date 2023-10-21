01893addf5a83e25cbeca5c5bddf1e2ee67efc9a
KillerSAA <136983879+KillerSAA@users.noreply.github.com>
2023-10-20 23:53:40 -0300

    Add files via upload

---
+++ includes/CWidget.h
@@ -0,0 +1,25 @@
+int pWidgets
+
+:SetWidgetPos
+    Memory.GetLabelPointer(pWidgets, @_ZN15CTouchInterface10m_pWidgetsE)
+    Memory.GetFuncPointerByCstrName(pWidgets, pWidgets)
+    pWidID *= 0x4
+
+    pWidgets += pWidID // sete o valor da variavel pWidID e das outras(de posição e escala) antes de chamar a função!                             
+        
+    Memory.Read(pWidgets, pWidgets, 4, False)
+    pWidgets += 0xC   
+    Memory.Write(pWidgets, fPosX, 4, False, True) // PosX
+    pWidgets += 0x4
+    Memory.Write(pWidgets, fPosY, 4, False, True) // PosY
+    pWidgets += 0x4
+    Memory.Write(pWidgets, fScaleX, 4, False, True) // scaleX
+    pWidgets += 0x4
+    Memory.Write(pWidgets, fScaleY, 4, False, True) // scaleY
+return 
+
+
+:_ZN15CTouchInterface10m_pWidgetsE
+hex
+    "_ZN15CTouchInterface10m_pWidgetsE" 00
+end
---
+++ includes/ShowText.h
@@ -0,0 +1,39 @@
+
+int addBigMsg, CreateGxt, textt, Gxt
+
+:ShowText    
+    0DD0: CreateGxt = get_label_addr @textToGxt  
+    0DD1: CreateGxt = get_func_addr_by_cstr_name CreateGxt 
+     
+    
+    0DD0: Gxt = get_label_addr @gxt 
+     
+    0DD3: context_set_reg 0 value textt // antes disso, a variavel textt deve receber o ponteiro para o texto.
+    0DD3: context_set_reg 1 value Gxt
+    0DD2: context_call_func CreateGxt
+      
+    
+    0DD0: addBigMsg = get_label_addr @func // android
+    0DD1: addBigMsg = get_func_addr_by_cstr_name addBigMsg // android 
+    
+    0DD3: context_set_reg 0 value Gxt // android 
+    0DD3: context_set_reg 1 value 5000 // android 
+    0DD3: context_set_reg 2 value 8 // android 
+    0DD2: context_call_func AddBigMsg // android 
+return
+                    
+:func 
+hex
+ "_ZN9CMessages13AddBigMessageEPtjt" 00
+end
+
+:textToGxt
+hex 
+ "_Z14AsciiToGxtCharPKcPt" 00
+end 
+
+
+:gxt 
+hex
+    00(32)
+end
